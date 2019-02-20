1c082f6071be1949938ce03f1e51fb9b0e8a31d6
manasye <manase32@gmail.com>
2019-02-21 06:24:59 +0700

    Add layout for login

---
+++ app/src/main/java/com/example/sip/Login.java
@@ -0,0 +1,33 @@
+package com.example.sip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Login extends AppCompatActivity {
+    private Button loginButton;
+    private EditText password, email;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginButton = findViewById(R.id.loginButton);
+        password = findViewById(R.id.passwordField);
+        email = findViewById(R.id.usernameField);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailValue = email.getText().toString();
+                String passwordValue = password.getText().toString();
+            }
+        });
+    }
+}
