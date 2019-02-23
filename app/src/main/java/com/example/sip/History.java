891c55640490730fae43fba817ce77da6ea25071
Jansen <13516011@std.stei.itb.ac.id>
2019-02-23 16:28:19 +0700

    Added recyclerView on History Fragment

--- app/src/main/java/com/example/sip/History.java
+++ app/src/main/java/com/example/sip/History.java
@@ -3,10 +3,14 @@ package com.example.sip;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -18,7 +22,7 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class History extends Fragment {
-
+    String[] strings = {"Hello1", "Hello2", "Hello3", "Hello4", "Hello5", "Hello6", "Hello7", "Hello8", "Hello1", "Hello2", "Hello3", "Hello4", "Hello5", "Hello6", "Hello7", "Hello8", "Hello1", "Hello2", "Hello3", "Hello4", "Hello5", "Hello6", "Hello7", "Hello8", "Hello1", "Hello2", "Hello3", "Hello4", "Hello5", "Hello6", "Hello7", "Hello8", "Hello1", "Hello2", "Hello3", "Hello4", "Hello5", "Hello6", "Hello7", "Hello8", "Hello1", "Hello2", "Hello3", "Hello4", "Hello5", "Hello6", "Hello7", "Hello8"};
     public History() {
 
     }
@@ -26,9 +30,43 @@ public class History extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history, container, false);
+        RecyclerView recyclerView = new RecyclerView(getContext());
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(new RVAdapter(strings));
+        return recyclerView;
     }
 
+    public class RVAdapter extends RecyclerView.Adapter<HistoryViewHolder> {
+        private String[] dataSource;
+        public RVAdapter(String[] dataArgs) {
+            this.dataSource = dataArgs;
+        }
 
+        @NonNull
+        @Override
+        public HistoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = new TextView(parent.getContext());
+            HistoryViewHolder viewHolder = new HistoryViewHolder(view);
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull HistoryViewHolder historyViewHolder, int position) {
+            historyViewHolder.textView.setText(dataSource[position]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return dataSource.length;
+        }
+    }
+
+    public static class HistoryViewHolder extends RecyclerView.ViewHolder {
+        public TextView textView;
+
+        public HistoryViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = (TextView) itemView;
+        }
+    }
 }
