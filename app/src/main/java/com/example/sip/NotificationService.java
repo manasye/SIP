48a96380772eab0c68f6b563fd2ded4dc96c97bb
Antonio Setya <antonio.armyn@gmail.com>
2019-02-25 13:25:16 +0700

    Alternate reminder text, based on user's daily goal

--- app/src/main/java/com/example/sip/NotificationService.java
+++ app/src/main/java/com/example/sip/NotificationService.java
@@ -8,12 +8,22 @@ import android.os.Build;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Map;
 
 public class NotificationService extends FirebaseMessagingService {
@@ -53,25 +63,65 @@ public class NotificationService extends FirebaseMessagingService {
     /* Show a notification to remind the user to walk (and meet it's daily target)
     *  TO_DO: Check whether the user already passed it's target or not */
     public void fireWalkReminder() {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,CHANNEL_ID);
-        builder.setSmallIcon(R.drawable.ic_launcher_background);
-        builder.setContentTitle("Get up and go!");
-        builder.setContentText("Walk around and be healthier!");
-        builder.setAutoCancel(true);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        String currentDate = sdf.format(Calendar.getInstance().getTime());
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference("users").child(user.getUid()).child("stepdata").child(currentDate);
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                int count, target;
+                if (dataSnapshot.child("target").exists()) {
+                    target = dataSnapshot.child("target").getValue(Integer.class);
+                } else {
+                    target = 0;
+                }
+                if (dataSnapshot.child("count").exists()) {
+                    count = dataSnapshot.child("count").getValue(Integer.class);
+                } else {
+                    count = 0;
+                }
 
-        NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                String contentTitle = "Get up and go!";
+                String contentText = "Walk around and be healthier!";
+                if (target != 0) {
+                    if (count >= target) {
+                        contentTitle = "Good job!";
+                        contentText = "You have reached your daily goal! Keep it up!";
+                    } else {
+                        contentText = " You are " + (target - count) + " steps away from reaching your daily goal!";
+                    }
+                }
 
-        /* For Oreo and above : need to create a notification channel first */
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel nc = new NotificationChannel(CHANNEL_ID, "S.I.P. Notification Channel", NotificationManager.IMPORTANCE_DEFAULT);
-            nm.createNotificationChannel(nc);
-            v.vibrate(VibrationEffect.createOneShot(200,VibrationEffect.DEFAULT_AMPLITUDE));
-        } else {
-            v.vibrate(200);
-        }
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(),CHANNEL_ID);
+                builder.setSmallIcon(R.drawable.ic_launcher_background);
+                builder.setContentTitle(contentTitle);
+                builder.setContentText(contentText);
+                builder.setAutoCancel(true);
 
-        nm.notify(0, builder.build());
+                NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+                /* For Oreo and above : need to create a notification channel first */
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    NotificationChannel nc = new NotificationChannel(CHANNEL_ID, "S.I.P. Notification Channel", NotificationManager.IMPORTANCE_DEFAULT);
+                    nm.createNotificationChannel(nc);
+                    v.vibrate(VibrationEffect.createOneShot(200,VibrationEffect.DEFAULT_AMPLITUDE));
+                } else {
+                    v.vibrate(200);
+                }
+
+                nm.notify(0, builder.build());
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
 
     }
 }
